2dddfb734f86be38ad71fe90651f93692781682c
deeedob <dennis.ob@protonmail.com>
2022-10-09 18:46:18 +0200

    cleaned up zsh; oh-my-zsh autoinstall

--- scripts/configuration.sh
+++ scripts/configuration.sh
@@ -13,4 +13,12 @@ sudo systemctl enable lightdm-plymouth.service
 
 gsettings set com.github.stunkymonkey.nautilus-open-any-terminal terminal alacritty
 
+# oh-my-zsh
+cd $(pwd)/files/.config/zsh/
+ZSH="$HOME/.config/zsh/oh-my-zsh" sh install.sh
+cd $ZSH/custom/plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting
+
+# setup user Account Image for LightDM
 echo "[USER]\nXSession=bspwm\nIcon=/var/lib/AccountsService/icons/$(whoami)\nSystemAccount=false" | sudo tee /var/lib/AccountsService/users/$(whoami)
