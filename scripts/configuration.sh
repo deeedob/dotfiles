2316a24ddf9d7cc3e8e783c110589f0d6c974431
deeedob <dennis.ob@protonmail.com>
2022-10-09 18:30:30 +0200

    rootfiles

--- scripts/configuration.sh
+++ scripts/configuration.sh
@@ -5,10 +5,12 @@ sudo usermod -aG adm $USER
 sudo usermod -aG wheel $USER
 sudo usermod -aG audio $USER
 
+sudo plymouth-set-default-theme -R lone
 
 sudo systemctl enable --now com.system76.PowerDaemon
 sudo systemctl enable bluetooth
+sudo systemctl enable lightdm-plymouth.service
 
 gsettings set com.github.stunkymonkey.nautilus-open-any-terminal terminal alacritty
-sudo ln -s /usr/bin/alacritty /usr/bin/xterm
 
+echo "[USER]\nXSession=bspwm\nIcon=/var/lib/AccountsService/icons/$(whoami)\nSystemAccount=false" | sudo tee /var/lib/AccountsService/users/$(whoami)
--- scripts/rootfiles.sh
+++ scripts/rootfiles.sh
@@ -7,10 +7,14 @@ echo "################"
 if [ -d files/root ] 
   then
     DIR=$(pwd)
-    echo "Current directory: ${DIR}"
     sudo ln -fvs ${DIR}/files/root/usr/share/icons/Bibata-Modern-TokyoNight /usr/share/icons/
     sudo ln -fvs ${DIR}/files/root/usr/share/icons/default/index.theme /usr/share/icons/default/
+    sudo ln -fvs ${DIR}/files/root/etc/zshenv /etc/zsh/
+    sudo ln -fvs ${DIR}/files/root/etc/lightdm/lightdm.conf /etc/lightdm/
+    sudo ln -fvs ${DIR}/files/root/username.jpg /var/lib/AccountsService/icons/$(whoami).jpg
+    sudo ln -fvs /usr/bin/alacritty /usr/bin/xterm # little trick
   else
     echo "AN ERROR OCURRED!"
+    exit -1
 fi
 
