4461054c97b5fe145cf4ee287e44050cb824226c
deeedob <dennis.ob@protonmail.com>
2022-10-14 22:11:13 +0200

    fix

--- scripts/configuration.sh
+++ scripts/configuration.sh
@@ -10,11 +10,10 @@ sudo systemctl enable lightdm-plymouth.service
 gsettings set com.github.stunkymonkey.nautilus-open-any-terminal terminal alacritty
 
 # oh-my-zsh
-cd $(pwd)/files/.config/zsh/
+cd ${BASEDIR}/files/.config/zsh/
 ZSH="$HOME/.config/zsh/oh-my-zsh" sh install.sh
-cd $ZSH/custom/plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.config/zsh/oh-my-zsh
+git clone https://github.com/zsh-users/zsh-syntax-highlighting $HOME/.config/zsh/oh-my-zsh
 
 # setup user Account Image for LightDM
 echo "[USER]\nXSession=bspwm\nIcon=/var/lib/AccountsService/icons/$(whoami)\nSystemAccount=false" | sudo tee /var/lib/AccountsService/users/$(whoami)
--- scripts/rootfiles.sh
+++ scripts/rootfiles.sh
@@ -6,12 +6,11 @@ echo "################"
 
 if [ -d files/root ] 
   then
-    DIR=$(pwd)
-    sudo ln -fvs ${DIR}/files/root/usr/share/icons/Bibata-Modern-TokyoNight /usr/share/icons/
-    sudo ln -fvs ${DIR}/files/root/usr/share/icons/default/index.theme /usr/share/icons/default/
-    sudo ln -fvs ${DIR}/files/root/etc/zshenv /etc/zsh/
-    sudo ln -fvs ${DIR}/files/root/etc/lightdm/lightdm.conf /etc/lightdm/
-    sudo ln -fvs ${DIR}/files/root/username.jpg /var/lib/AccountsService/icons/$(whoami).jpg
+    sudo ln -fvs ${BASEDIR}/files/root/usr/share/icons/Bibata-Modern-TokyoNight /usr/share/icons/
+    sudo ln -fvs ${BASEDIR}/files/root/usr/share/icons/default/index.theme /usr/share/icons/default/
+    sudo ln -fvs ${BASEDIR}/files/root/etc/zshenv /etc/zsh/
+    sudo ln -fvs ${BASEDIR}/files/root/etc/lightdm/lightdm.conf /etc/lightdm/
+    sudo ln -fvs ${BASEDIR}/files/root/username.jpg /var/lib/AccountsService/icons/$(whoami).jpg
     sudo ln -fvs /usr/bin/alacritty /usr/bin/xterm # little trick
   else
     echo "AN ERROR OCURRED!"
