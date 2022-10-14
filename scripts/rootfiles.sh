87fd51094e83c1648509761e317373d27ffb95b2
deeedob <dennis.ob@protonmail.com>
2022-10-14 23:32:48 +0200

    symlinked to encrypted home ... blunder

--- scripts/rootfiles.sh
+++ scripts/rootfiles.sh
@@ -9,7 +9,7 @@ if [ -d files/root ]
     sudo ln -fvs ${BASEDIR}/files/root/usr/share/icons/Bibata-Modern-TokyoNight /usr/share/icons/
     sudo ln -fvs ${BASEDIR}/files/root/usr/share/icons/default/index.theme /usr/share/icons/default/
     sudo ln -fvs ${BASEDIR}/files/root/etc/zshenv /etc/zsh/
-    sudo ln -fvs ${BASEDIR}/files/root/etc/lightdm/lightdm.conf /etc/lightdm/
+    sudo cp ${BASEDIR}/files/root/etc/lightdm/lightdm.conf /etc/lightdm/
     sudo ln -fvs ${BASEDIR}/files/root/username.jpg /var/lib/AccountsService/icons/$(whoami).jpg
     sudo ln -fvs /usr/bin/alacritty /usr/bin/xterm # little trick
   else
