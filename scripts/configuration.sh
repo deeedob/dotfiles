272083d322e2eaeb12d9458976cc5115662a7578
deeedob <dennis.ob@protonmail.com>
2022-10-16 18:10:42 +0200

    fixes on live vm

--- scripts/configuration.sh
+++ scripts/configuration.sh
@@ -11,9 +11,14 @@ gsettings set com.github.stunkymonkey.nautilus-open-any-terminal terminal alacri
 
 # oh-my-zsh
 cd ${BASEDIR}/files/.config/zsh/
-ZSH="$HOME/.config/zsh/oh-my-zsh" sh install.sh
-git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.config/zsh/oh-my-zsh
-git clone https://github.com/zsh-users/zsh-syntax-highlighting $HOME/.config/zsh/oh-my-zsh
+if [ ! -d $HOME/.config/zsh/oh-my-zsh ]; then
+  echo "Downloading oh-my-zsh"
+  ZSH="$HOME/.config/zsh/oh-my-zsh" sh install.sh
+fi
+
+echo "Installing zsh plugins"
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.config/zsh/oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting $HOME/.config/zsh/oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 # setup user Account Image for LightDM
 echo "[USER]\nXSession=bspwm\nIcon=/var/lib/AccountsService/icons/$(whoami)\nSystemAccount=false" | sudo tee /var/lib/AccountsService/users/$(whoami)
