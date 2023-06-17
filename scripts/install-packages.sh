1423436d5925b0a2890c5402565c8b7e4c8685d5
deeedob <dennis.ob@protonmail.com>
2023-06-17 17:54:37 +0200

    packages: add obsidian

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -4,7 +4,7 @@ base="fish ranger neovim-nightly btop git-revise meld"
 dev="qtcreator clion clang llvm docker git-open nodejs"
 sys="alacritty nextcloud-client keepassxc solaar wine wine-gecko wine-mono teams-for-linux flatpak"
 wayland="wl-clipboard wev dunst polkit-kde-agent pipewire wireplumber qt5-wayland qt6-wayland hyprpaper dunst xdg-desktop-portal-hyprland-git rofi-lbonn-wayland"
-media="bitwig-studio renoise-demo supercollider discord-screenaudio"
+media="bitwig-studio renoise-demo supercollider discord-screenaudio obsidian"
 appearance="ttf-jetbrains-mono-nerd"
 
 echo "Using: yay -Syu --needed --noconfirm $base $dev $sys $media $appearance $wayland"
