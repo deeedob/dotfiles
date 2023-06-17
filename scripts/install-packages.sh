ada4d9ba0c42fee5eb8a75d33828428474f21e5f
deeedob <dennis.ob@protonmail.com>
2023-06-17 19:46:04 +0200

    Introduce: eww as a desktop bar

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -6,10 +6,11 @@ sys="alacritty nextcloud-client keepassxc solaar wine wine-gecko wine-mono teams
 wayland="wl-clipboard wev dunst polkit-kde-agent pipewire wireplumber qt5-wayland qt6-wayland hyprpaper dunst xdg-desktop-portal-hyprland-git rofi-lbonn-wayland"
 media="bitwig-studio renoise-demo supercollider discord-screenaudio obsidian"
 appearance="ttf-jetbrains-mono-nerd"
+eww="eww-wayland jaq brillo jc ttf-material-design-icons otf-jost"
 
-echo "Using: yay -Syu --needed --noconfirm $base $dev $sys $media $appearance $wayland"
+echo "Using: yay -Syu --needed --noconfirm $base $dev $sys $media $appearance $wayland $eww"
 
-yay -Syu --needed --noconfirm "$base" "$dev" "$sys" "$media" "$appearance" "$wayland"
+yay -Syu --needed --noconfirm "$base" "$dev" "$sys" "$media" "$appearance" "$wayland" "$eww"
 
 systemctl start docker
 systemctl enable docker
