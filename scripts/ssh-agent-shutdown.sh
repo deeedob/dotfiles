119feea1732ff82ed1775fccb54c3b3cd4e98213
deeedob <dennis.ob@protonmail.com>
2023-06-18 17:20:09 +0200

    Remove old KDE startup and shutdown files

--- scripts/ssh-agent-shutdown.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-[ -z "$SSH_AGENT_PID" ] || eval "$(ssh-agent -k)"
--- scripts/ssh-agent-startup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-[ -n "$SSH_AGENT_PID" ] || eval "$(ssh-agent -s)"
-SSH_ASKPASS=/usr/bin/ksshaskpass
-export SSH_ASKPASS
