b64c1eaaa38c95969e6859eb7bc31080a32fef1e
deeedob <dennis.ob@protonmail.com>
2023-07-10 00:19:33 +0200

    config: update Qt and install files

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -2,7 +2,7 @@
 
 base="fish ranger neovim-nightly btop git-revise meld"
 dev="qtcreator clion clang llvm docker git-open nodejs"
-sys="alacritty plasma-systemmonitor kdeplasma-addons nextcloud-client keepassxc solaar wine wine-gecko wine-mono teams-for-linux flatpak kwin-bismuth unclutter ksshaskpass"
+sys="alacritty clamav plasma-systemmonitor kdeplasma-addons nextcloud-client keepassxc solaar wine wine-gecko wine-mono teams-for-linux flatpak kwin-bismuth unclutter ksshaskpass"
 media="bitwig-studio renoise-demo supercollider discord-screenaudio"
 appearance="kdeplasma-themes-kde-gruvbox-git klassy gtk-engine-murrine ttf-jetbrains-mono-nerd"
 
