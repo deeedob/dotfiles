a7b7390d3639f3eda1ad39ad6fe348f1d79b605d
deeedob <dennis.ob@protonmail.com>
2023-07-12 22:52:19 +0200

    Rename dotbot and res dir; rework install script

---
+++ notes.md
@@ -0,0 +1,9 @@
+# Notes
+
+> Slow startup time of gtk based applications:
+
+```
+# KDE
+sudo pacman -Rdd xdg-desktop-portal-gnome
+sudo pacman -S xdg-desktop-portal-kde
+```
--- setup.sh
+++ setup.sh
@@ -2,15 +2,7 @@
 
 echo "Setting up the system."
 
-# Check if the default shell is already fish
-if [[ $SHELL == "/usr/bin/fish" ]]; then
-    echo "The default shell is already set to fish."
-else
-    chsh -s /usr/bin/fish
-    echo "The default shell has been set to fish."
-fi
-
 cursor_theme="Bibata-Modern-TokyoNight"
 if [ ! -d /usr/share/icons/${cursor_theme} ]; then
-    sudo cp -r ../res/${cursor_theme} /usr/share/icons/
+    sudo cp -r .res/${cursor_theme} /usr/share/icons/
 fi
