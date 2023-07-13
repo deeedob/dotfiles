731d1d85d2c177e93f492841ad3f6cdf960b98e2
deeedob <dennis.ob@protonmail.com>
2023-07-13 21:24:19 +0200

    Update sys files

--- sys-setup.sh
+++ sys-setup.sh
@@ -2,8 +2,8 @@
 
 base="fish ranger neovim-nightly btop git-revise meld"
 dev="qtcreator clion clang llvm docker git-open nodejs"
-sys="alacritty nextcloud-client keepassxc solaar wine wine-gecko wine-mono teams-for-linux json-c dart-sass"
-media="bitwig-studio renoise-demo supercollider discord-screenaudio obsidian"
+sys="alacritty nextcloud-client keepassxc solaar wine wine-gecko wine-mono teams-for-linux json-c dart-sass qt6qtk2 qt6ct"
+media="bitwig-studio renoise-demo supercollider discord-screenaudio obsidian soulseek"
 appearance="ttf-jetbrains-mono-nerd ttf-material-design-icons"
 
 wayland="wl-clipboard wev dunst polkit-kde-agent pipewire wireplumber qt5-wayland qt6-wayland hyprpaper dunst xdg-desktop-portal-hyprland-git rofi-lbonn-wayland"
