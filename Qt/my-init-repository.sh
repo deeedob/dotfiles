0a19c01f2e51f05e65014d24e219bf02c1975c6a
deeedob <dennis.ob@protonmail.com>
2023-07-20 11:32:54 +0200

    Qt: Update user presets create a my-init-repo script

---
+++ Qt/my-init-repository.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# use inside Qt master repo directory
+./init-repository --module-subset=qtbase,qtdeclarative,qtdoc,qtgrpc,qtlanguageserver,qtmultimedia,qtquickeffectmaker,qtremoteobjects,qtrepotools,qtshadertools,qtsvg,qttools,qtwayland,qtquick3d,qtconnectivity --codereview-username dennisoberst -f
