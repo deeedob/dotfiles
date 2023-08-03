eafcea03ee2589f8bed501686bfe5891b2c9e588
deeedob <dennis.ob@protonmail.com>
2023-08-03 21:27:19 +0200

    setup.sh: Enable ptrace globally

--- setup.sh
+++ setup.sh
@@ -13,6 +13,12 @@ else
     $HOME/Dotfiles/.third-party/symlink_themes.sh
 fi
 
+# enable ptrace for gdb globally
+ptrace_file="/etc/sysctl.d/10-ptrace.conf"
+if [ ! -f "$ptrace_file" ]; then
+    echo "kernel.yama.ptrace_scope = 0" | sudo tee "$ptrace_file"
+fi
+
 # install xremap and set is as service to get custom keybinds.
 sudo make -C .config/xremap 1> /dev/null
 
