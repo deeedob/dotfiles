3d8ee3fcbe8ed4928c814ec3714b7a8a265f839c
deeedob <dennis.ob@protonmail.com>
2024-08-09 13:34:28 +0200

    updated hyprland for new desktop setup

--- .config/nvim/TODO.md
+++
@@ -1,8 +0,0 @@
-# Improvements
-
-- Cmp Search
-- Cmp Show fn args
-- Cmp styling
-- Remove Lspsaga?
-
-- Command/fn to quickly open added "impportant fiels", like this TODO here
--- Scripts/find_unlinked.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Replace the path to a required library:
-# patchelf --replace-needed ../libsomething1.so /foo/bar/libsomething1.so mysharedobject.so
-for file in *.so
-do
-    ldd $file | grep "not found"
-done
