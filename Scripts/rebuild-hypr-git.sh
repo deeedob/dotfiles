00dd26c79d6b90405160bb8bb5cc3427c20d5632
deeedob <dennis.ob@protonmail.com>
2025-06-18 11:41:35 +0200

    Add hypr rebuild script

---
+++ Scripts/rebuild-hypr-git.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+yay -S \
+    hyprland-git \
+    hyprland-protocols-git \
+    hyprland-qt-support-git \
+    hyprland-qtutils-git \
+    hyprlang-git \
+    hyprlock-git \
+    hyprcursor-git \
+    hyprgraphics-git \
+    hypridle-git \
+    hyprshot-git \
+    hyprutils-git \
+    hyprpaper-git \
+    hyprpicker-git \
+    hyprpolkitagent-git \
+    hyprwayland-scanner-git \
+    xdg-desktop-portal-hyprland-git \
+    aquamarine-git
