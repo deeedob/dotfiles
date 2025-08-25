befb79afe67e44e78619a79fa42b44ec450df3e0
deeedob <dennis.ob@protonmail.com>
2025-08-25 17:16:14 +0200

    improve Qt setup and remove unused aliases

--- Qt/setup.sh
+++ Qt/setup.sh
@@ -1,24 +1,56 @@
 #!/bin/bash
 
+set -e
+
 if [ ! -f "./init-repository" ]; then
     echo "init-repository not found in current directory!"
     exit 1
 fi
 
-cmake_base_preset=~/Dotfiles/Qt/CMakePresets.json
-if [ ! -f $cmake_base_preset ]; then
-    echo "Did not find in $cmake_base_preset!"
+cmake_repo_preset=~/Qt/CMakePresets.json
+cmake_master_preset=~/Qt/CMakeMasterPresets.json
+
+if [ ! -f "$cmake_repo_preset" ]; then
+    echo "Repo preset not found: $cmake_repo_preset"
     exit 1
 fi
-git submodule foreach "ln -svrf '$cmake_base_preset' . || true"
 
-read -rp "Update Qt to dev? [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
-    git submodule foreach "git checkout dev || true"
-    git submodule foreach "git pull || true"
+if [ ! -f "$cmake_master_preset" ]; then
+    echo "Master preset not found: $cmake_master_preset"
+    exit 1
 fi
 
-read -rp "Do you want to install the dependencies? [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
-    yay -S --needed --noconfirm lttng-ust libxcb xdb-proto xcb-util xcb-util-image xcb-util-wm libxi renderdoc-bin mysql postgresql oracle-instantclient-sdk unixodbc ccache mariadb llvm gperf python-html5lib fontconfig openxr libwmf
+ln -sf "$cmake_master_preset" CMakePresets.json
+
+base_repos=(qtrepotools qtbase qtdeclarative qtimageformats qtshadertools qtsvg qtgrpc qttools)
+for repo in "${base_repos[@]}"; do
+    git submodule update --init --recursive "$repo"
+    ln -sf "$cmake_repo_preset" "$repo/CMakePresets.json"
+done
+
+read -rp "Checkout and update all repos to a specific branch (e.g. dev)? [empty to skip]: " branch
+if [ -n "$branch" ]; then
+    echo "Checking out branch '$branch'..."
+
+    git fetch origin "$branch"
+    git checkout -B "$branch" "origin/$branch" || echo "Main repo: branch '$branch' not found"
+
+    git submodule foreach "
+        if [ \"\$name\" != \"qtrepotools\" ]; then
+            git fetch origin $branch
+            git checkout -B $branch origin/$branch || echo \"\$name: branch '$branch' not found\"
+            git submodule update --init --recursive
+        fi"
 fi
+
+if [[ "$(uname)" == "Linux" ]]; then
+    read -rp "Do you want to install the dependencies? [y/N] " response
+    if [[ "$response" =~ ^[yY]([eE][sS])?$ ]]; then
+        yay -S --needed --noconfirm \
+            lttng-ust libxcb xdb-proto xcb-util xcb-util-image \
+            xcb-util-wm libxi renderdoc-bin mysql postgresql \
+            oracle-instantclient-sdk unixodbc ccache mariadb \
+            llvm gperf python-html5lib fontconfig openxr libwmf
+    fi
+fi
+
